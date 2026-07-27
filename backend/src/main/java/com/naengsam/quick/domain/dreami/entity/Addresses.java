ea811c85740fc05cf60081ca0b099e7ca53c57bd
dlsnfl0615 <hyunseo0750@gmail.com>
2026-07-27 17:41:37 +0900

    BE/Feat : 주소 객체 선언
    - 기본주소, 상세주소, 위도, 경도, 별명
    - 출발지와 도착지 각각에 대해서 가짐

---
+++ backend/src/main/java/com/naengsam/quick/domain/dreami/entity/Addresses.java
@@ -0,0 +1,19 @@
+package com.naengsam.quick.domain.dreami.entity;
+
+import lombok.Builder;
+
+@Builder
+public record Addresses(
+        String originAddressLine1, // 기본주소
+        String originAddressLine2, // 상세주소
+        String originLatitude,
+        String originLongitude,
+        String originAlias,
+
+        String destinationAddressLine1,
+        String destinationAddressLine2,
+        String destinationLatitude,
+        String destinationLongitude,
+        String destinationAlias
+) {
+}
