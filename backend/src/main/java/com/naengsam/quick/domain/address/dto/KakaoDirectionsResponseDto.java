c6dd186c28ecc966847a75c06ab27ff7a48e6811
hwhyeons <vvpass507@gmail.com>
2026-08-09 12:40:58 +0900

    BE/Feat : Kakao API응답 KakaoDirectionsResponseDto에서 실제 이동 경로도 받아오게 추가

--- backend/src/main/java/com/naengsam/quick/domain/address/dto/KakaoDirectionsResponseDto.java
+++ backend/src/main/java/com/naengsam/quick/domain/address/dto/KakaoDirectionsResponseDto.java
@@ -12,7 +12,47 @@ public record KakaoDirectionsResponseDto(
 ) {
     @JsonIgnoreProperties(ignoreUnknown = true)
     public record Route(
-            Properties properties
+            Properties properties, // 전체 경로 속성
+            Leg[] legs // 경로 구간 목록
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Leg(
+            LegProperties properties, // 경로 구간 속성
+            Step[] steps // 경로 구간 단계 목록
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record LegProperties(
+            int distance, // 경로 구간 거리(단위: 미터(m))
+            int time // 경로 구간 소요 시간(단위: 초(s))
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Step(
+            StepProperties properties, // 경로 단계 속성
+            Path path // 경로 단계 좌표 정보
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record StepProperties(
+            int distance, // 경로 단계 거리(단위: 미터(m))
+            String guidance, // 경로 단계 안내 문구
+            int time, // 경로 단계 소요 시간(단위: 초(s))
+            double x, // 경로 단계 시작점 X 좌표
+            double y // 경로 단계 시작점 Y 좌표
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Path(
+            // 경로 단계 좌표 목록, 내부 배열은 [x, y] 형식의 좌표 쌍
+            // (예: [[127.02700693, 37.49864277], [127.02698289, 37.49863151]])
+            double[][] points
     ) {
     }
 
