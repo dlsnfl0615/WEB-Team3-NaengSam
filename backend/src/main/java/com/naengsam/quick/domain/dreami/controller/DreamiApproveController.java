17817567052a6001247a781cec7a5771a8bb809b
dlsnfl0615 <hyunseo0750@gmail.com>
2026-08-16 13:51:49 +0900

    BE/Refactor : 관리자 페이지 컨트롤러 이름 변경

--- backend/src/main/java/com/naengsam/quick/domain/dreami/controller/DreamiApproveController.java
+++ backend/src/main/java/com/naengsam/quick/domain/dreami/controller/DreamiApproveController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/debug/dreami-review")
 @RequiredArgsConstructor
-public class DreamiReviewDebugController {
+public class DreamiApproveController {
 
     private final DreamiService dreamiService;
 
